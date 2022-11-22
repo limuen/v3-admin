f350038d5970ebb4d5856254717e9fff5c1cba1a
limuen <limuen_dev@163.com>
2022-11-22 10:40:30 +0800

    first commit

---
+++ src/layouts/components/Footer/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="footer flx-center">
+		<a href="https://github.com/HalseySpicy" target="_blank"> 艾阳 </a>
+	</div>
+</template>
+
+<style scoped lang="scss">
+@import "./index.scss";
+</style>
---
+++ src/layouts/components/Header/ToolBarRight.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="tool-bar-ri">
+		<div class="header-icon">
+			<AssemblySize id="assemblySize" />
+			<Language id="language" />
+			<ThemeSetting id="themeSetting" />
+			<Fullscreen id="fullscreen" />
+		</div>
+		<span class="username">Liam</span>
+		<Avatar />
+	</div>
+</template>
+
+<script setup lang="ts">
+import Fullscreen from "./components/Fullscreen.vue";
+import Language from "./components/Language.vue";
+import ThemeSetting from "./components/ThemeSetting.vue";
+import AssemblySize from "./components/AssemblySize.vue";
+import Avatar from "./components/Avatar.vue";
+</script>
+
+<style scoped lang="scss">
+.tool-bar-ri {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin: 0 30px;
+	.header-icon {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		width: 160px;
+		margin-right: 22px;
+	}
+	.username {
+		margin: 0 20px 0 0;
+		font-size: 15px;
+	}
+}
+</style>
